9bf1c01aa4bc4dec337c231b4a0a7e0079f57fe5
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-01-23 12:58:11 +0530

    [1267] [Time Beats: 66.67%] [Memory Beats: 60.67%] - LeetPush

---
+++ 1267-Count-Servers-that-Communicate.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        rows = [0]*m
+        cols = [0]*n
+
+        res =0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] ==1:
+                    rows[i]+=1
+                    cols[j]+=1
+
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1 and (rows[i] >1 or cols[j] >1):
+                    res+=1
+        return res
+
+        
