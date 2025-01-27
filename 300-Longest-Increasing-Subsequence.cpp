42e6bce947a1365436c9c684692515b25615436c
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-01-27 10:35:56 +0530

    [300] [Time Beats: 50.66%] [Memory Beats: 71.83%] - LeetPush

---
+++ 300-Longest-Increasing-Subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> t(n,1);
+        int maxLis =1;
+
+        for(int i =0;i<n;i++){
+            for(int j =0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    t[i]= max(t[i], t[j]+1);
+                }
+                maxLis= max(maxLis,t[i]);
+            }
+        }
+        return maxLis;
+        
+    }
+};
