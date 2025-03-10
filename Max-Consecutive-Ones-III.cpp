e1b68c95a7473a710f7c35f928098370ab094341
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-03-10 20:57:58 +0530

    [1004] [Time Beats: 100.00%] [Memory Beats: 25.82%] - LeetPush

---
+++ Max-Consecutive-Ones-III.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        int left = 0, right = 0, length = 0, maxLength = 0, zeros = 0;
+        while(right<n){
+            if(nums[right] == 0) zeros++;
+            if(zeros>k){
+                if(nums[left] == 0) zeros--;
+                left++;
+            }
+            if(zeros<=k){
+                length = right - left + 1;
+                maxLength = max(length, maxLength);
+            }
+            right++;
+        }
+        return maxLength;
+        
+    }
+};
