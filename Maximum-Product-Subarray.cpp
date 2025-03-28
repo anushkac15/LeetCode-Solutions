dec0d7438b62c61fef8330b4ba5c2c34dd1d7151
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-03-28 18:17:19 +0530

    [152] [Time Beats: 100.00%] [Memory Beats: 57.34%] - LeetPush

--- Maximum-Product-Subarray.cpp
+++ Maximum-Product-Subarray.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
 
-        int pro1 = nums[0];
-        int pro2 = nums[0];
-        int res =nums[0];
+        int prefix =1;
+        int suffix =1;
+        int res = INT_MIN;
 
-        for(int i =1;i< nums.size();i++){
-            int temp = max({nums[i], pro1*nums[i], pro2*nums[i]});
-            pro2 = min({nums[i], pro1*nums[i], pro2*nums[i]});
-            pro1 = temp;
-            res = max(pro1, res);
+        for(int i =0;i<nums.size();i++){
+
+            if(prefix==0){
+                prefix =1;
+            }
+            if(suffix==0){
+                suffix =1;
+            }
+            prefix *= nums[i];
+            suffix *= nums[nums.size()-i-1];
+
+
+            res = max(res, max(prefix, suffix));
         }
         return res;
         
