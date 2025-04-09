1753c6eadad0209b346fbd85c7c635c4100a6aac
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-04-09 14:38:03 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 48.83%] - LeetPush

---
+++ Climbing-Stairs.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    int solve(int n, vector<int>&dp){
+
+            if(n<0){
+                return 0;
+            }
+
+            if(n==1 || n==0){
+                return 1;
+            }
+
+
+            if(dp[n]!=-1){
+                return dp[n];
+            }
+
+            int oneStep = solve(n-1,dp);
+            int twoStep = solve(n-2,dp);
+
+
+            return dp[n] = oneStep+twoStep;
+
+    }
+    int climbStairs(int n) {
+
+        vector<int>dp(n+1, -1);
+
+        return solve(n,dp);
+
+        
+    }
+};
