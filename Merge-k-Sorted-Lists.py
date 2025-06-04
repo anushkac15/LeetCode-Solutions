16b85e16a8062dea97609dd93e545993e0ce7a58
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-06-05 02:26:52 +0530

    [23] [Time Beats: 70.84%] [Memory Beats: 36.36%] - LeetPush

--- Merge-k-Sorted-Lists.py
+++ Merge-k-Sorted-Lists.py
@@ -1,45 +1,28 @@
+import heapq
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
 
-        def TwoSortedLists(list1, list2):
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
 
-            dummy = ListNode()
-            res = dummy
-            while list1 and list2:
-                if list1.val <list2.val:
-                    res.next = list1
-                    list1= list1.next
+        dummy = ListNode()
+        current = dummy
 
-                else:
-                    res.next = list2
-                    list2=list2.next
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            current.next = node
+            current = current.next
 
-                res = res.next
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
 
-            if list1:
-                res.next = list1
-            if list2:
-                res.next = list2
-
-            return dummy.next
-
-            
-        if not lists or len(lists) == 0:
-            return None
-        
-        while len(lists) > 1:
-            temp = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i+1] if i + 1 < len(lists) else None
-                temp.append(TwoSortedLists(l1, l2))
-            lists = temp
-        
-        return lists[0]
-
-        
+        return dummy.next
