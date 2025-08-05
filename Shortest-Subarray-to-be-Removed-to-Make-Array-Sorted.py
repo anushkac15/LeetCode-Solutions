29687eb4d9565713e23deb4dd136535063a63e87
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-08-05 20:57:51 +0530

    [1574] [Time Beats: 97.98%] [Memory Beats: 15.32%] - LeetPush

---
+++ Shortest-Subarray-to-be-Removed-to-Make-Array-Sorted.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+
+        j =len(arr)-1
+        while (j>0 and arr[j]>=arr[j-1]):
+            j-=1
+
+        ans = j
+        i=0
+
+        while(i<j and(i==0 or arr[i-1]<=arr[i])):
+            while(j<len(arr) and arr[i]>arr[j]):
+                j+=1
+            
+            ans =min(ans, j-i-1)
+            i+=1
+
+        return ans
