e7dfcb60eeaaa164126ef7eb648f43bef8ced013
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-08-30 20:46:05 +0530

    [199] [Time Beats: 100.00%] [Memory Beats: 60.64%] - LeetPush

---
+++ Binary-Tree-Right-Side-View.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+
+        def solve(root,level, res):
+
+            if root is None:
+                return 
+
+            if(level==len(res)):
+                res.append(root.val)
+
+            solve(root.right,level+1,res)
+            solve(root.left,level+1,res)
+
+        res =[]
+        solve(root,0,res)
+        return res
