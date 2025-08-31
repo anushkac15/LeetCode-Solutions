f3a15b6fcfe4d2251ce1f2e56814d3130dd4672c
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-08-31 15:03:15 +0530

    [19] [Time Beats: 100.00%] [Memory Beats: 54.64%] - LeetPush

--- Remove-Nth-Node-From-End-of-List.py
+++ Remove-Nth-Node-From-End-of-List.py
@@ -10,13 +10,12 @@ class Solution:
         dummy = res
 
         for _ in range(n):
+            head=head.next
+
+        while head:
             head = head.next
+            dummy= dummy.next
 
-        while head :
-            head = head.next
-            dummy = dummy.next
-
-        dummy .next = dummy.next.next
-
+        dummy.next = dummy.next.next
         return res.next
         
