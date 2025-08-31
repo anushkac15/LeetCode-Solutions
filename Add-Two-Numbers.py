dbae1e8193c5be5a0176e13cbedfe0efa6964238
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-08-31 16:05:23 +0530

    [2] [Time Beats: 77.54%] [Memory Beats: 35.43%] - LeetPush

--- Add-Two-Numbers.py
+++ Add-Two-Numbers.py
@@ -6,8 +6,8 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        temp = ListNode()
-        res = temp
+        res = ListNode()
+        dummy = res
 
         carry =0
 
@@ -16,14 +16,14 @@ class Solution:
             l1Val = l1.val if l1 else 0
             l2Val = l2.val if l2 else 0
 
-            total = l1Val+l2Val+ carry
-            carry = total//10
-            digit = total%10
+            sum = l1Val+l2Val+carry
+            carry=sum//10
+            digit =sum%10
 
-            temp.next = ListNode(digit)
-            temp = temp.next
+            res.next = ListNode(digit)
+            res=res.next
 
-            if l1: l1=l1.next
-            if l2 : l2 = l2.next
+            if l1: l1 = l1.next
+            if l2:  l2=l2.next
 
-        return res.next
+        return dummy.next
