369a2abf9fee9971e1eb911c05ffe59dab1b8fec
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-09-19 21:20:27 +0530

    [1574] [Time Beats: 75.64%] [Memory Beats: 58.72%] - LeetPush

--- Shortest-Subarray-to-be-Removed-to-Make-Array-Sorted.py
+++ Shortest-Subarray-to-be-Removed-to-Make-Array-Sorted.py
@@ -3,18 +3,18 @@ class Solution:
 
         j = len(arr)-1
 
-        while j >0 and arr[j]>= arr[j-1]:
+        while j >0 and arr[j] >=arr[j-1]:
             j-=1
 
         ans =j
-        i =0
+        i=0
 
-        while (i<j and (i==0 or arr[i]>= arr[i-1])):
+        while (i<j and (i==0 or arr[i]>=arr[i-1])):
             while(j<len(arr) and arr[i]>arr[j]):
                 j+=1
 
             ans = min(ans, j-i-1)
             i+=1
-        
+
         return ans
         
