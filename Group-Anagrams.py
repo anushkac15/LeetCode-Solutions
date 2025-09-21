30a00076835a76f1257aac917766a8d00fe0cb02
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2025-09-21 15:33:47 +0530

    [49] [Time Beats: 82.63%] [Memory Beats: 62.54%] - LeetPush

--- Group-Anagrams.py
+++ Group-Anagrams.py
@@ -4,9 +4,8 @@ class Solution:
         mp = defaultdict(list)
 
         for s in strs:
-            key = \\.join(sorted(s))
 
-            mp[key].append(s)
-        
+            mp[''.join(sorted(s))].append(s)
+
         return list(mp.values())
         
