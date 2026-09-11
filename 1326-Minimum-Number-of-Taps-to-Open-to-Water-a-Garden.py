8366d09165affb3af7d3bbc23b20fea6da4debd1
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-11 17:33:58 +0530

    [1326] [Time Beats: 98.45%] [Memory Beats: 72.52%] - LeetPush

--- 1326-Minimum-Number-of-Taps-to-Open-to-Water-a-Garden.py
+++ 1326-Minimum-Number-of-Taps-to-Open-to-Water-a-Garden.py
@@ -1,24 +1,30 @@
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        max_range = [0] * (n + 1)
-        
+
+        startEnd = [0]*(n+1)
+
         for i in range(len(ranges)):
-            left = max(0, i - ranges[i])
-            right = min(n, i + ranges[i])
-            max_range[left] = max(max_range[left], right)
+
+            left = max(0, i-ranges[i])
+            right = min(i+ranges[i], n)
+
+            startEnd[left] = max(startEnd[left], right)
+
+        maxInd =0
+        currInd =0
+        taps =0
+
+        for i in range(len(ranges)):
+
+            if i>maxInd :
+                return -1
+
+            if i>currInd:
+                currInd = maxInd
+                taps +=1
+
+            maxInd = max(maxInd, startEnd[i])
+
+        return taps
+                
         
-        end = farthest = taps = 0
-        i = 0
-        
-        while end < n:
-            while i <= end:
-                farthest = max(farthest, max_range[i])
-                i += 1
-            
-            if farthest <= end:
-                return -1  # Unable to cover the entire garden
-            
-            end = farthest
-            taps += 1
-        
-        return taps        
