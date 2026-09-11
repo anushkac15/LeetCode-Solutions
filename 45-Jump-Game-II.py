e30df4ffadd7482bc9a0ed1e28a730a07d62f876
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-11 17:40:44 +0530

    [45] [Time Beats: 89.06%] [Memory Beats: 15.88%] - LeetPush

---
+++ 45-Jump-Game-II.py
@@ -0,0 +1,20 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+
+        maxInd =0
+        currInd =0
+        jump =0
+
+        for i in range(len(nums)-1):
+
+            maxInd = max(maxInd, i+nums[i])
+
+            if i==currInd :
+                currInd = maxInd 
+                jump+=1
+
+            if i>=len(nums):
+                break
+
+        return jump
