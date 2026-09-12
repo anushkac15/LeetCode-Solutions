e759c4d987cd985307fc02f3a678515abff20b9c
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-13 01:05:08 +0530

    [1326] [Time Beats: 95.11%] [Memory Beats: 90.13%] - LeetPush

--- 1326-Minimum-Number-of-Taps-to-Open-to-Water-a-Garden.py
+++ 1326-Minimum-Number-of-Taps-to-Open-to-Water-a-Garden.py
@@ -6,25 +6,23 @@ class Solution:
         for i in range(len(ranges)):
 
             left = max(0, i-ranges[i])
-            right = min(i+ranges[i], n)
+            right = min(i+ranges[i], len(ranges))
 
             startEnd[left] = max(startEnd[left], right)
 
-        maxInd =0
-        currInd =0
-        taps =0
+        maxEnd =0
+        currEnd =0
+        taps=0
 
         for i in range(len(ranges)):
 
-            if i>maxInd :
+            if i>maxEnd :
                 return -1
 
-            if i>currInd:
-                currInd = maxInd
-                taps +=1
+            if i>currEnd:
+                taps+=1
+                currEnd = maxEnd
 
-            maxInd = max(maxInd, startEnd[i])
+            maxEnd = max(maxEnd, startEnd[i])
 
         return taps
-                
-        
