c5106033043819a8d30727c98de6ec858d56ee5c
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-14 15:23:21 +0530

    [322] [Time Beats: 10.22%] [Memory Beats: 24.74%] - LeetPush

--- 322-Coin-Change.py
+++ 322-Coin-Change.py
@@ -1,12 +1,22 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0  
-        
-        for i in range(1, amount + 1):
-            for coin in coins:
-                if i - coin >= 0:
-                    dp[i] = min(dp[i], dp[i - coin] + 1)
-        
-        return dp[amount] if dp[amount] != float('inf') else -1   
+        def solve(i, amount, dp):
+
+            if amount == 0:
+                return 0
+
+            if i >= len(coins) or amount < 0:
+                return float("inf")
+
+            if dp[i][amount] != -1:
+                return dp[i][amount]
+
+            take = solve(i, amount - coins[i], dp) + 1
+            notTake = solve(i + 1, amount, dp)
+
+            dp[i][amount] = min(take, notTake)
+            return dp[i][amount]
+
+        dp = [[-1] * (amount + 1) for _ in range(len(coins))]
+        return solve(0, amount, dp) if solve(0, amount, dp) != float("inf") else -1
