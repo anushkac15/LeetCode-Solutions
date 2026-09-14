fabb62679cdde2779f9a5be1e6126bc38d161a1e
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-14 15:33:06 +0530

    [152] [Time Beats: 90.41%] [Memory Beats: 23.00%] - LeetPush

---
+++ 152-Maximum-Product-Subarray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+
+        pre = 0
+        suff = 0
+
+        maxi = float("-inf")
+
+        for i in range(len(nums)):
+
+            if pre == 0:
+                pre = 1
+            if suff == 0:
+                suff = 1
+
+            pre *= nums[i]
+            suff *= nums[len(nums) - i - 1]
+
+            maxi = max(maxi, max(pre, suff))
+
+        return maxi
