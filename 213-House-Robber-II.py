16e129d482ced03c7b24a26a1859cd6a29590050
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-14 14:20:00 +0530

    [213] [Time Beats: 100.00%] [Memory Beats: 11.34%] - LeetPush

--- 213-House-Robber-II.py
+++ 213-House-Robber-II.py
@@ -6,22 +6,20 @@ class Solution:
             if i>end:
                 return 0
 
-            if dp[i]!=-1:
+            if dp[i] !=-1:
                 return dp[i]
 
-            take = solve(i+2,end, dp)+nums[i]
-            notTake= solve(i+1,end,dp)
+            take = solve(i+2,end, dp) + nums[i]
+            notTake = solve(i+1,end, dp)
 
             dp[i] = max(take, notTake)
             return dp[i]
 
-        dp1 = [-1]*len(nums)
-        dp2 = [-1] *len(nums)
-
         if len(nums) ==1:
             return nums[0]
 
-        return max(solve(0,len(nums)-2, dp1), solve(1, len(nums)-1, dp2))
+        dp1 = [-1]*len(nums)
+        dp2 = [-1]*len(nums)
 
-            
+        return max(solve(0,len(nums)-2,dp1), solve(1, len(nums)-1, dp2))
         
