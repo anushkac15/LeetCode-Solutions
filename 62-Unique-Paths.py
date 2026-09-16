095d017ad9fb22f510099a6065316f64bc845c10
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-16 13:00:15 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 43.94%] - LeetPush

--- 62-Unique-Paths.py
+++ 62-Unique-Paths.py
@@ -3,21 +3,21 @@ class Solution:
 
         def solve(i, j, dp):
 
-            if i>=m or j>=n:
-                return 0
-
-            if i==m-1 and j==n-1:
+            if i == m - 1 or j == n - 1:
                 return 1
 
-            if dp[i][j] !=-1:
+            if i >= m and j >= n:
+                return 0
+
+            if dp[i][j] != -1:
                 return dp[i][j]
 
-            right = solve(i+1, j, dp)
-            down = solve(i, j+1, dp)
+            right = solve(i + 1, j, dp)
+            down = solve(i, j + 1, dp)
 
             dp[i][j] = right + down
+
             return dp[i][j]
 
-        dp = [[-1]*(n+1) for _ in range(m+1)]
-        return solve(0,0,dp)
-        
+        dp = [[-1] * n for _ in range(m)]
+        return solve(0, 0, dp)
