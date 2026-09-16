22c0f71091bcc2bfcc5cec7dd892c38d784b9a3f
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-16 17:56:57 +0530

    [347] [Time Beats: 89.90%] [Memory Beats: 80.90%] - LeetPush

--- 347-Top-K-Frequent-Elements.py
+++ 347-Top-K-Frequent-Elements.py
@@ -1,17 +1,17 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
 
         mp = defaultdict(int)
 
-        for n in nums:
-            mp[n]+=1
+        res = []
 
-        heap =[]
+        for n in nums:
+            mp[n] += 1
+
+        heap = []
 
         for n, cnt in mp.items():
-            heapq.heappush(heap,(-cnt, n))
-
-        res=[]
+            heapq.heappush(heap, (-cnt, n))
 
         for _ in range(k):
             res.append(heapq.heappop(heap)[1])
