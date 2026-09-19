2eb23c8c0475ae40f362fcc0fd9da446d86619b8
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-19 14:47:42 +0530

    [1899] [Time Beats: 78.17%] [Memory Beats: 85.91%] - LeetPush

--- 1899-Merge-Triplets-to-Form-Target-Triplet.py
+++ 1899-Merge-Triplets-to-Form-Target-Triplet.py
@@ -1,9 +1,16 @@
 class Solution:
     def mergeTriplets(self, triplets: list[list[int]], target: list[int]) -> bool:
-        a = b = c = False
+
+        t1 = False
+        t2 = False
+        t3 = False
+
         for t in triplets:
+
             if t[0] <= target[0] and t[1] <= target[1] and t[2] <= target[2]:
-                a = a or t[0] == target[0]
-                b = b or t[1] == target[1]
-                c = c or t[2] == target[2]
-        return a and b and c
+
+                t1 = t1 or (t[0] == target[0])
+                t2 = t2 or (t[1] == target[1])
+                t3 = t3 or (t[2] == target[2])
+
+        return t1 and t2 and t3
